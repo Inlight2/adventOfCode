df7e1558c4ea3129408b54681db7e7f1fa9310da
Kenneth Cox <kennethCox5@gmail.com>
2015-12-20 12:39:42 -0800

    there's an issue with my parsing somewhere, or my understanding of the problem

---
+++ AdventOfCode/Assets/Day8/DayEight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+public class DayEight : ReadBase {
+
+	void Start() {
+		int characters = 0;
+		foreach(string stri in reader.lines) {
+			string str = stri.Substring (1, stri.Length - 2);
+			str = Regex.Replace (str, "\\\\\"", "@");
+			str = Regex.Replace (str, "\\\\\\\\", "?");
+			str = Regex.Replace (str, @"\\x[0-9a-f]{2,4}", @"^");
+			Debug.Log (str);
+			characters += stri.Length - str.Length;
+		}
+
+		Debug.Log (characters);
+	}
+}
