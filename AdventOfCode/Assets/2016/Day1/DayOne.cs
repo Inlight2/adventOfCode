793da0915a50df440a8d53f924b7f0bc2b80c26d
Kenneth Cox <kennethCox5@gmail.com>
2016-12-03 00:30:58 -0800

    made a new folder for 2016 and did 3 advents

---
+++ AdventOfCode/Assets/2016/Day1/DayOne.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace yearTwo
+{
+	enum Direction
+	{
+		NORTH = 0,
+		EAST = 1,
+		SOUTH = 2,
+		WEST = 3,
+	};
+
+	public class DayOne : ReadBase {
+
+		Direction dir = Direction.NORTH;
+		Point p = new Point(0,0);
+		Dictionary<string, int> visitCount = new	Dictionary<string, int>();
+
+		// Use this for initialization
+		void Start () {
+			//Calculate the path and ending point
+			foreach (string str in reader.lines) {
+				if (str [0].Equals ('R')) {
+					TurnRight ();
+				} else {
+					TurnLeft ();
+				}
+
+				if (MoveSingle (int.Parse (str.Substring(1)))) {
+						Debug.Log ("First crossed position at: " + p.ToString ());
+						Debug.Log("Distance from start is :" + (Mathf.Abs(p.x) + Mathf.Abs(p.y)));
+						return;
+				}
+				Debug.Log ("Moved "+ dir + " to " + p.ToString ());
+			}
+
+			//determine distance from starting position
+			Debug.Log("Final position: " + p.ToString());
+			Debug.Log("Distance from start is :" + (Mathf.Abs(p.x) + Mathf.Abs(p.y)));
+		}
+
+		void TurnLeft() {
+			dir--;
+			if (dir < Direction.NORTH) {
+				dir = Direction.WEST;
+			}
+		}
+
+		void TurnRight() {
+			dir++;
+			if (dir > Direction.WEST) {
+				dir = Direction.NORTH;
+			}
+		}
+
+		void Move(int num) {
+			switch (dir) {
+
+			case Direction.NORTH:
+				p.y += num;
+				break;
+			case Direction.EAST:
+				p.x += num;
+				break;
+			case Direction.SOUTH:
+				p.y -= num;
+				break;
+			case Direction.WEST:
+				p.x -= num;
+				break;
+
+			default:
+				break;
+			}
+		}
+
+		bool MoveSingle(int num) {
+
+			for(int i = 0; i < num; i++) {
+
+				switch (dir) {
+
+				case Direction.NORTH:
+					p.y++;
+					break;
+				case Direction.EAST:
+					p.x++;
+					break;
+				case Direction.SOUTH:
+					p.y--;
+					break;
+				case Direction.WEST:
+					p.x--;
+					break;
+
+				default:
+					break;
+				}
+
+				if (VisitPosition ())
+					return true;
+			}
+			return false;
+		}
+
+		bool VisitPosition() {
+			int count;
+			if (visitCount.TryGetValue (p.ToString(), out count)) {
+				visitCount [p.ToString ()] = count++;
+				return true;
+			}
+
+			visitCount.Add(p.ToString(), 1);
+
+			return false;
+		}
+	}
+}
---
+++ AdventOfCode/Assets/2016/Day2/DayTwo.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace yearTwo
+{
+	public class DayTwo : ReadBase {
+
+		int curButton = 5;
+
+		void Start () {
+			foreach (string str in reader.lines) {
+				foreach (char chr in str) {
+					if(chr.Equals('U')){
+						Up();
+					} else if(chr.Equals('D')){
+						Down();
+					} else if(chr.Equals('L')){
+						Left();
+					} else if(chr.Equals('R')){
+						Right();
+					}
+					//Debug.Log(chr.ToString() + " to " + curButton);
+				}
+				Debug.Log("EndPoint:" + curButton);
+			}
+		}
+
+		void Up() {
+			int t = curButton;
+			curButton -= 3;
+			if( curButton < 1) {
+				curButton = t;
+			}
+		}
+
+		void Down() {
+			int t = curButton;
+			curButton += 3;
+			if(curButton > 9) {
+				curButton = t;
+			}
+		}
+
+		void Left() {
+			curButton--;
+			if( curButton < 1) {
+				curButton = 1;
+			} else if( curButton == 3) {
+				curButton = 4;
+			} else if( curButton == 6) {
+				curButton = 7;
+			}
+		}
+
+		void Right() {
+			curButton++;
+			if( curButton == 4) {
+				curButton = 3;
+			} else if( curButton == 7) {
+				curButton = 6;
+			} else if( curButton > 9) {
+				curButton = 9;
+			}
+		}
+	}
+}
